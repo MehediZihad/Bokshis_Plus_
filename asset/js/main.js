c6891d982486d8b2d5cd23d9899dcedf806e2bca
mehedizihad <mehedi.zihad@wayne.edu>
2022-10-25 11:10:26 -0400

    Commit updated files

---
+++ asset/js/main.js
@@ -0,0 +1,68 @@
+$(function () {
+    /*==========   Mobile Menu   ==========*/
+    $('.navbar-toggler').on('click', function () {
+        $(this).toggleClass('actived');
+    })
+    $('.navbar-toggler').on('click', function () {
+        $('.navbar-collapse').toggleClass('menu-opened');
+    })
+
+    // Toggle dropdown Menu in Mobile
+    $('.dropdown-menu [data-toggle=dropdown]').on('click', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $(this).parent().siblings().removeClass("opened");
+        $(this).parent().toggleClass("opened");
+    });
+    $('.dropdown-submenu [data-toggle=dropdown]').on('click', function (e) {
+        $(this).next().toggleClass("show");
+        $(this).parent().siblings().find('.dropdown-menu').removeClass('show');
+    });
+
+    /*==========   Sticky Navbar   ==========*/
+    $(window).on('scroll', function () {
+        if ($(window).width() >= 992) {
+            var $navbar = $('.sticky-navbar');
+            if ($(window).scrollTop() > 100) {
+                $navbar.addClass('fixed-navbar');
+            } else {
+                $navbar.removeClass('fixed-navbar');
+            }
+        }
+    });
+
+    /*==========   Increase and Decrease Input Value   ==========*/
+    // Increase Value
+    $('.increase-qty').on('click', function () {
+        var $qty = $(this).parent().find('.qty-input');
+        var currentVal = parseInt($qty.val());
+        if (!isNaN(currentVal)) {
+            $qty.val(currentVal + 1);
+        }
+    });
+
+    // Decrease Value
+    $('.decrease-qty').on('click', function () {
+        var $qty = $(this).parent().find('.qty-input');
+        var currentVal = parseInt($qty.val());
+        if (!isNaN(currentVal) && currentVal > 1) {
+            $qty.val(currentVal - 1);
+        }
+    });
+
+    /*==========   Range Slider  ==========*/
+    var $rangeSlider = $("#rangeSlider"),
+        $rangeSliderResult = $("#rangeSliderResult");
+    $rangeSlider.slider({
+        range: true,
+        min: 0,
+        max: 300,
+        values: [50, 200],
+        slide: function (event, ui) {
+            $rangeSliderResult.val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $rangeSliderResult.val("$" + $rangeSlider.slider("values", 0) + " - $" + $rangeSlider.slider("values", 1));
+
+});
+
